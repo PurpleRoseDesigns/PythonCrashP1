a60f52f83430427951487c9ab00a5213f8b234b6
Laura Snow <101505908+PurpleRoseDesigns@users.noreply.github.com>
2025-08-08 12:57:12 +0100

    minor: creating drop and movement for aliens

--- alien.py
+++ alien.py
@@ -5,8 +5,8 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
-        # self.settings = ai_game.settings
-        # self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+       
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
@@ -14,44 +14,18 @@ class Alien(Sprite):
         self.rect.y = self.rect.height
 
         self.x = float(self.rect.x)
-        
-        # start each new ship bottom-center of screen
-        # self.rect.top = self.screen_rect.top
-
-        # Below code sets the image in the center of screen
-        # self.rect.midbottom = self.screen_rect.center
-
-        # Store decimal value for ship horizontal position
-        # self.x = float(self.rect.x)
-        # self.y = float(self.rect.y)
-
-        # Movement Flags
-        # self.moving_right = False
-        # self.moving_left = False
-
-        # Movement Flags for up and down, not used in current game
-        # self.moving_up = False
-        # self.moving_down = False
-
+              
             
-    # def update(self):
-    #     '''Update Ship position using movement flags'''
-    #     if self.moving_right and self.rect.right < self.screen_rect.right:
-    #         self.x += self.settings.alien_speed
-    #     if self.moving_left and self.rect.left > 0:
-    #         self.x -= self.settings.alien_speed
+    def check_edges(self):
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
 
-        # Up and Down positioning using movement flags, not used in current game
-        # if self.moving_up and self.rect.top > 0:
-        #     self.y -= self.settings.ship_speed
-        # if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
-        #     self.y += self.settings.ship_speed
-
+    def update(self):
+        '''Update Alien position using movement flags'''
+       
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+       
         # update rect object from self.x
-        # self.rect.x = self.x
+        self.rect.x = self.x
 
-        # update rect object for y axis, not used in current game
-        # self.rect.y = self.y
-
-    # def blitme(self):
-    #     self.screen.blit(self.image, self.rect)
